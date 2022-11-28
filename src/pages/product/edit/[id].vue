56a19e3f90821e727f5273442aab5da678111899
Billy Maulana <billymaulana1999@gmail.com>
2022-11-28 18:01:13 +0700

    style: add placeholders

--- src/pages/product/edit/[id].vue
+++ src/pages/product/edit/[id].vue
@@ -54,44 +54,43 @@ const submitUpdateProduct = async () => {
           </h1>
           <form w-full mx-auto block mt-5 @submit.prevent="submitUpdateProduct">
             <div class="mb-5">
-              <label>Product Name</label>
-              <input v-model="productData.name" class="form-control" type="text">
+              <label font-medium text-black>Product Name</label>
+              <input v-model="productData.name" class="form-control" type="text" placeholder="Product Name">
             </div>
             <div class="mb-5">
-              <label>Product description</label>
-              <textarea v-model="productData.description" class="form-control h-full" type="text" cols="5" rows="5" />
+              <label font-medium text-black>Product description</label>
+              <textarea v-model="productData.description" class="form-control h-full" type="text" cols="5" rows="5" placeholder="Description" />
             </div>
             <div class="mb-5">
-              <label>Product Image: URL</label>
-              <input v-model="productData.image" class="form-control" type="text">
+              <label font-medium text-black>Product Image: URL</label>
+              <input v-model="productData.image" class="form-control" type="text" placeholder="URL Image">
             </div>
             <div class="mb-5">
-              <label>Product Price:</label>
+              <label font-medium text-black>Product Price:</label>
               <input v-model="productData.price" class="form-control" type="text">
             </div>
             <div class="mb-5">
-              <label>Product Width:</label>
+              <label font-medium text-black>Product Width:</label>
               <input v-model="productData.width" class="form-control" type="number">
             </div>
             <div class="mb-5">
-              <label>Product length:</label>
+              <label font-medium text-black>Product length:</label>
               <input v-model="productData.length" class="form-control" type="number">
             </div>
             <div class="mb-5">
-              <label>Product Weight:</label>
+              <label font-medium text-black>Product Weight:</label>
               <input v-model="productData.weight" class="form-control" type="number">
             </div>
             <div class="mb-5">
-              <label>SKU Code</label>
-              <input v-model="productData.sku" class="form-control" type="text">
+              <label font-medium text-black>SKU Code</label>
+              <input v-model="productData.sku" class="form-control" type="text" placeholder="SKU Code">
             </div>
             <div class="mb-5">
-              <label>Category Id:</label>
+              <label font-medium text-black>Category Id:</label>
               <input v-model.number="productData.categoryId" class="form-control" type="number">
             </div>
             <div class="mb-5">
-              <label>Category Name:</label>
-              <input v-model.number="productData.categoryName" class="form-control" type="text">
+              <label font-medium text-black>Category Name:</label>
             </div>
             <button type="submit" btn-primary mt-5 :disabled="disableSubmit" :class="disableSubmit ? 'text-gray' : 'text-white'">
               Submit
